dc4978edc91e958cacfcd7dcd4f87b4654d92907
kfh53 <kyle.f.harris53@gmail.com>
2026-08-04 17:11:41 -0400

    Updated url for forgot

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -18,7 +18,7 @@ export default function ForgotPasswordPage() {
         setIsSubmitting(true);
 
         const { error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: `${window.location.origin}/reset-password`
+            redirectTo: "https://parlay-project.vercel.app/update-password"
         });
         setIsSubmitting(false);
 
