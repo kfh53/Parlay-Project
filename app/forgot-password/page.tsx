c90e869f810cb36373c2572f394c53de70b27401
kfh53 <kyle.f.harris53@gmail.com>
2026-08-04 18:58:31 -0400

    Added auth for password reset

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -18,7 +18,7 @@ export default function ForgotPasswordPage() {
         setIsSubmitting(true);
 
         const { error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: "https://parlay-project.vercel.app/update-password"
+            redirectTo: "https://parlay-project.vercel.app/auth/callback?next=/update-password"
         });
         setIsSubmitting(false);
 
